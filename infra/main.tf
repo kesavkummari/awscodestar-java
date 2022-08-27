0ba59e56f253f6bd8d5517c57480fbef4d22dfc6
Kesav Kummari <96249106+kesavkummari@users.noreply.github.com>
2022-08-27 11:51:56 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,12 +1,8 @@
-# Terraform AWS Provider Versions
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
+provider "aws" {
+    region = "us-east-1"
+    profile = "default"
 }
+
 
 # Resource
 resource "aws_instance" "webserver" {
