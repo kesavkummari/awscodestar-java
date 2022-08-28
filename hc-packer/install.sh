3bac20ee61e31e6d92bad6cdb262f1fbd26f6dfa
c.kummari <c.kummari@outlook.com>
2022-08-28 12:51:47 +0530

    Packer Code Update

--- hc-packer/install.sh
+++ hc-packer/install.sh
@@ -1,3 +1,5 @@
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install -i /usr/local/aws-cli -b /usr/local/bin
+# curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+# unzip awscliv2.zip
+# sudo ./aws/install -i /usr/local/aws-cli -b /usr/local/bin
+
+sudo systemctl restart snap.amazon-ssm-agent.amazon-ssm-agent.service
